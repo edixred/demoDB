eb012266c995e8c2273af0428b86fec05688bd82
bayron.champutiz@clave.cl <bayron.champutiz@clave.cl>
2024-03-07 09:17:54 -0500

    update folder

---
+++ SCRIPT-2024-03-03/2024-03-03-0001-example.sql
@@ -0,0 +1,9 @@
+create table pais (
+  nombre varchar(30),
+  codigo varchar(10)
+ );
+create table municipio (
+  nombre varchar(30),
+  codigo varchar(10),
+  region varchar(10)
+ );
---
+++ SCRIPT-2024-03-03/2024-03-03-0002-example.sql
@@ -0,0 +1,10 @@
+
+create table horario (
+  codigo varchar(30),
+  fecha date,
+  horas integer
+ );
+create table departamento (
+  nombre varchar(30),
+  codigo varchar(10)
+ );
