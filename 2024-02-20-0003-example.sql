cd1ac78af9209fccb873e5eaf1e4ff00eb3e7c72
edixred <edixred@hotmail.com>
2024-03-07 07:49:23 -0500

    Update 2024-02-20-0003-example.sql

--- 2024-02-20-0003-example.sql
+++ 2024-02-20-0003-example.sql
@@ -1,1 +1,11 @@
-
+create table producto (
+  codigo varchar(30),
+  fecha_vencimiento date,
+  descripcion varchar(50),
+  cantidad integer
+ );
+create table sucursal (
+  codigo varchar(30),
+  nombre varchar(30),
+  descripcion varchar(50)
+ );
